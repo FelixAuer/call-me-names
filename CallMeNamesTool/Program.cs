81972af918d4613fd4a83009a4f20fe8dd86164e
Felix Auer <felix.auer@trustbit.tech>
2023-03-06 11:15:21 +0100

    Initial Commit

---
+++ CallMeNamesTool/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CallMeNamesTool;
+
+internal class Program
+{
+    private static readonly List<string> Names = new() { "Seppi", "Peppi", "Tom", "Jerry", "Heppo", "Peppo" };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("I'm now calling you " + Names[(new Random().Next(Names.Count))]);
+    }
+}
